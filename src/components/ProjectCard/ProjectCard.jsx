defde349961e8a68225514879319499a7a547379
DBStirling <davidstirling17@gmail.com>
2024-10-20 00:40:50 -0400

    added hover states to project card, graphics & collage

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,45 +1,42 @@
-import React, { useState } from 'react'
-import {Link} from 'react-router-dom'
-import ProjectTag from '../ProjectTag/ProjectTag.jsx'
-import './ProjectCard.css'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import ProjectTag from '../ProjectTag/ProjectTag.jsx';
+import './ProjectCard.css';
 
 export default function ProjectCard(props) {
   const { title, description, tag, destination, imgSrc, last } = props;
-
-  const [isHovered, setIsHovered] = useState(false)
-
-  const cardContainer = document.querySelector('.card-container');
-
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
     <div className='see-more-container'>
-      <div className='card-container'
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}>
+      <Link
+        to={destination}
+        className='card-container'
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        style={{ textDecoration: 'none', color: 'inherit' }} // Keeps text styles consistent
+      >
         <div className='card-contents'>
-        <div className='card-header'>
-          <div className='title'>
-            <Link
-              className='card-header'
-              to={destination}
-            >{title}</Link>
+          <div className='card-header'>
+            <div className='title'>{title}</div>
+            <div className='project-tag'>
+              <ProjectTag tag={tag} />
+            </div>
           </div>
-          <div className='project-tag'>
-          <ProjectTag 
-            tag={tag}
-          />
+          <p className='description'>
+            {description}
+          </p>
+        </div>
+        <img src={imgSrc} alt={`${title} Thumbnail`} className='thumbnail' />
+        {last && (
+          <div className="end-work-prompt">
+            That's all! See more in&nbsp;
+            <Link to="/about" className="end-work-link">About</Link>
+            &nbsp;or&nbsp;
+            <Link to="/extras" className="end-work-link">Extras</Link>.
           </div>
-        </div>
-        <p className='description'>
-          {description}
-        </p>
-        </div>
-        <img src={imgSrc} alt={`${title} Thumbnail`}/>
-        {last 
-        && <div className="end-work-prompt">
-              That's all! See more in&nbsp;<Link to="/about" className="end-work-link">About</Link>&nbsp;or&nbsp;<Link to='/extras' className="end-work-link">Extras</Link>.
-            </div>}
-      </div>
+        )}
+      </Link>
     </div>
-  )
+  );
 }
