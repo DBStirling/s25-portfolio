abdceebc51a4280aedf485e5f45a1d13d14de4a2
DBStirling <davidstirling17@gmail.com>
2024-10-25 23:20:15 -0400

    de summary cards made. 1 & 2 done

--- src/pages/donEfficace.jsx
+++ src/pages/donEfficace.jsx
@@ -1,13 +1,55 @@
 import React from 'react'
+import { useState } from 'react'
+import ModalImage from "react-modal-image";
 import CtaButton from '../components/CtaButton/CtaButton';
 import Divider from '../components/Divider/Divider';
 import ThanksSection from '../components/ThanksSection/ThanksSection.jsx'
 import miniMFP from '../assets/Images/mini-de-thumb-simple.png'
 import miniDM from '../assets/Images/mini-dm-thumb-simple.png'
+import formPair from '../assets/Images/de/form-pair.png'
+import taxDetail from '../assets/Images/de/tax-potential-detail.png'
+import impactCard from '../assets/Images/de/impact-card.png'
+import donorPair from '../assets/Images/de/donor-pair.png'
+import donorDetail from '../assets/Images/de/donor-detail.png'
+import summaryDetail from '../assets/Images/de/summary-card.png'
+
+import { IoIosArrowUp } from "react-icons/io";
 import '../styles/caseStudy.css'
 
 export default function donEfficace() {
-  return (
+
+    const [buttonOneDescription, setButtonOneDescription] = useState('Learn More')
+    const [buttonTwoDescription, setButtonTwoDescription] = useState('Learn More')
+    const [buttonThreeDescription, setButtonThreeDescription] = useState('Learn More')
+
+    const [showItemOne, setShowItemOne] = useState(false)
+    const [showItemTwo, setShowItemTwo] = useState(false)
+    const [showItemThree, setShowItemThree] = useState(false)
+
+    const toggleItem = (input) => {
+        if (input === 'one') {
+            setShowItemOne((prevState) => {
+                const newState = !prevState;
+                setButtonOneDescription(newState ? 'Hide details' : 'Learn more');
+                return newState;
+            });
+        } else if (input === 'two') {
+            setShowItemTwo((prevState) => {
+                const newState = !prevState;
+                setButtonTwoDescription(newState ? 'Hide details' : 'Learn more');
+                return newState;
+            });
+        } else if (input === 'three') {
+            setShowItemThree((prevState) => {
+                const newState = !prevState;
+                setButtonThreeDescription(newState ? 'Hide details' : 'Learn more');
+                return newState;
+            });
+        }
+    };
+
+
+return (
     <div className='device-container'>
         <div className="main-content-case">
             <div className="sub-section introduction">
@@ -58,9 +100,10 @@ export default function donEfficace() {
             </div>
             </div>
             <div className="sub-section key-pain-points">
-            <Divider
-            title={'key pain points'}/>
-            <div className="key-pain-points-body don-efficace">
+                <Divider
+                    title={'key pain points'}
+                />
+                <div className="key-pain-points-body don-efficace">
                 <div className='key-pain-point-header'>
                         <div className='heading'>At the end of the day, <em>we define success as...</em></div>
                     <div className="pain-point ht-pair">
@@ -93,8 +136,115 @@ export default function donEfficace() {
                         </p>
                     </div>
                 </div>
+                </div>
             </div>
-        </div>
+            <div className='sub-section key-pain-points'>
+                <Divider
+                    title={'the solution'}
+                />
+                <div className='sub-section key-pain-points-body don-efficace'>
+                    <div className='de-summary-item'>
+                        <div className='de-summary-description'>
+                            <div className='de-summary-contents'>
+                                <div className='heading'>Donation Form</div>
+                                <body>We designed a donation form that estimates a donation’s impact, using empathy and recognition to build confidence in donations. We also leveraged French tax breaks in our designs to compel users to donate more.</body>
+                                <button className='learn-more-button'
+                                    onClick={() => toggleItem('one')} // Wrap it in an arrow function
+                                    >
+                                    <body>{buttonOneDescription}</body>
+                                </button>
+                            </div> 
+                            <ModalImage
+                                className='summary-img'
+                                small={formPair}
+                                large={formPair}
+                                alt="Donation Form"
+                                hideDownload={true}
+                            />
+                        </div>
+                        {showItemOne && (
+                            <div className='item-description-container'>
+                                <div className='description-item'>
+                                    <div className='ht-pair'>
+                                        <body><b>1. Leveraging Tax Breaks</b></body>
+                                        <body>One of the key pain points relayed to us by Don Efficace was that donor’s didn’t know how to take advantage of tax breaks when donating. As a result, we baked the functionality of France’s tax break system into our donation form. By opting to using tax potential, users set the amount that they want to donate and they are recommended a donation value that maximizes their impact and costs them the same amount after taxes are returned.</body>
+                                    </div>
+                                    <img
+                                        className='full-width-detail'
+                                        src={taxDetail}
+                                    />
+                                </div>
+                                <div className='description-item-row'>
+                                    <div className='ht-pair'>
+                                        <body><b>2. Bridging the Empathy Gap</b></body>
+                                        <body>One of the leading reasons for the decline in donations is the public’s declining faith in NPOs and their inability to assess how much good their donation can do. To combat this, we designed additional transparency and constant feedback in our form, showing impact estimations for every donation. By showing users exactly where their money is going, we believe that we can remind them of why they’re donating and give them an extra push through finalizing their donation.</body>
+                                    </div>
+                                    <img
+                                        className='in-line-detail'
+                                        src={impactCard}
+                                    />
+                                </div>
+                                <div className='description-item'>
+                                    <div className='ht-pair'>
+                                        <body><b>3. Valuing the user</b></body>
+                                        <body>We made sure that the donation experience was not only cohesive but extremely transparent. Especially with the integration of a new feature like tax potential, we ensured that the form was straightforward to avoid startling or catching the user off guard.</body>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                    <div className='de-summary-item'>
+                        <div className='de-summary-description'>
+                            <div className='de-summary-contents'>
+                                <div className='heading'>Donor Portal</div>
+                                <body>We built a donor portal that allows users to manage reoccurring donations, access their donation history and all of their necessary tax documentation. We also consolidated their information into a donor dashboard for at-a-glance stats and metrics.</body>
+                                <button className='learn-more-button'
+                                    onClick={() => toggleItem('two')} // Wrap it in an arrow function
+                                    >
+                                    <body>{buttonTwoDescription}</body>
+                                </button>
+                            </div> 
+                            <ModalImage
+                                className='summary-img'
+                                small={donorPair}
+                                large={donorPair}
+                                alt="Donation Form"
+                                hideDownload={true}
+                            />
+                        </div>
+                        {showItemTwo && (
+                            <div className='item-description-container'>
+                                <div className='description-item'>
+                                    <div className='ht-pair'>
+                                        <body><b>1. Making Donation Details Accessible</b></body>
+                                        <body>One of my first tasks on the project was to create the donation history page, allowing users to download tax receipts for each of their donations and manage their reoccurring payments. We identified 3 core filtering criteria through conversations with the client and put together a donation details table for users. One of the best decisions we made throughout the whole design process, was investing time into building the table modularly as we reused the style across many donor and admin-facing pages, saving us plenty of headaches.</body>
+                                    </div>
+                                    <img
+                                        className='full-width-detail'
+                                        src={donorDetail}
+                                    />
+                                </div>
+                                <div className='description-item-row'>
+                                    <div className='description-item'>
+                                        <div className='ht-pair'>
+                                            <body><b>2. Reminding them Why They Donate</b></body>
+                                            <body>As donors continue to contribute, it was essential to remind them of their real-world impact as donation fatigue is a very real threat. We implemented an all-time impact module on their donation dashboard to show donors their growing impact and encourage them to continue donating.</body>
+                                        </div>
+                                        <div className='ht-pair'>
+                                            <body><b>3. Account Management</b></body>
+                                            <body>Finally, we built an account management page that allows the user to update their tax-registered address, email address and other personal details, extending the long-term usability of our platform. Furthermore, we also implemented a secure flow for updating a user’s password, integrating existing frameworks to lighten the development workload.</body>
+                                        </div>
+                                    </div>
+                                    <img
+                                        className='in-line-detail'
+                                        src={summaryDetail}
+                                    />
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 )
