bc61cd678b3eeb9e23342a3cf39b78a920ad673c
DBStirling <davidstirling17@gmail.com>
2024-10-26 19:13:52 -0400

    added /BrowserRouter to main.jsx import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom/BrowserRouter";
 import App from './App.jsx'
 import './index.css'
 
