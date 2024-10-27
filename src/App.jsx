888d0d339de9abb174fffa3ba8320881246abe80
DBStirling <davidstirling17@gmail.com>
2024-10-26 21:57:23 -0400

    attempt to use hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import React, { useRef, Suspense, lazy, useEffect } from "react";
-import { useState } from 'react'
-import { Routes, Route, useLocation  } from 'react-router-dom';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import NavBar from './components/NavBar/NavBar.jsx'
-import Footer from './components/Footer/Footer.jsx'
-// import Home from './pages/home.jsx'
-// import About from './pages/about.jsx'
-// import Extras from './pages/extras.jsx'
-// import MyFitnessPal from './pages/myFitnessPal'
-// import DietMe from './pages/dietMe.jsx'
-import LoadingPage from './pages/loadingPage.jsx'
-import './styles/App.css'
-
+import { useState } from 'react';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import LoadingPage from './pages/loadingPage.jsx';
+import './styles/App.css';
 
 const Home = lazy(() => import('./pages/home.jsx'));
 const About = lazy(() => import('./pages/about.jsx'));
@@ -61,7 +53,6 @@ function App() {
                                 </div>
                                 <Footer />
                             </Suspense>
-
                         </div>
                     )}
                 </div>
