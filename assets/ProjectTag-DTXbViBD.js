5ba657134123ee3281f8f6e66df17daee471fffe
DBStirling <davidstirling17@gmail.com>
2024-10-26 20:58:05 -0400

    Updates

--- assets/ProjectTag-DTXbViBD.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-IrKwkJLy.js";function o(e){const{tag:s,destination:i}=e;var a;return s=="Product"?a="product":s=="Exploration"?a="exploration":s=="Case Study"&&(a="case-study"),t.jsxs("div",{className:"tag-container",children:[t.jsx("div",{className:`circle ${a}`}),t.jsx("div",{className:"tag",children:s})]})}export{o as P};
--- assets/caseStudy-X_pKUcSj.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-IrKwkJLy.js";import"./Divider-CJwU9hqX.js";function a(c){const{text:i,clickFunctio:n}=c;return t.jsx("div",{className:"cta-container",onClick:n,children:t.jsx("div",{className:"cta-text",children:`${i}`})})}export{a as C};
