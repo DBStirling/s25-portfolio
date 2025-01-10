cb6e0272bdba8dfa85c1ccec82f791358dd95162
DBStirling <davidstirling17@gmail.com>
2025-01-10 01:34:37 -0500

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import ReactDOM from 'react-dom/client';
+import { Analytics } from "@vercel/analytics/react"
 import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <BrowserRouter>
           <App />
+          <Analytics/>
       </BrowserRouter>
   </React.StrictMode>
 );
